e83a9909dd58797b3b0b92c4429cf947945abd98
Matilda578 <tilly.g@hotmail.com>
2020-06-17 13:18:26 +1000

    all

---
+++ quiz.js
@@ -0,0 +1,10 @@
+const form = document.forms.pikachu;
+const radios = form.elements.pikachu;
+const value = radios.value;
+
+let radios,checked,value = 'answer';
+if (answer == 'Mouse') {
+    console.log("Yes"), document.body.innerHTML = "Woo"   ;
+} else {
+    console.log("No");
+}
